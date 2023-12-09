27cb171b6ae49cb5d1406f5ccce4f6a74a9ab2e8
Khathathon Nissayan <143895455+MickieProjects@users.noreply.github.com>
2023-12-09 20:06:47 +0700

    code update

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log('Hello Todo');
+const inputEl = document.getElementById('input');
+const formEl = document.querySelector('form');
+const listEl = document.getElementById('list');
+formEl.addEventListener('submit', saveData);
+const tasks = readData();
+tasks.forEach(createList);
+// save
+function saveData(event) {
+    event.preventDefault();
+    const newTask = {
+        name: inputEl.value,
+        completed: false
+    };
+    createList(newTask);
+    tasks.push(newTask);
+    localStorage.setItem("myList", JSON.stringify(tasks));
+}
+// createList
+function createList(task) {
+    const liEl = document.createElement("li");
+    const checkboxEl = document.createElement("input");
+    checkboxEl.type = "checkbox";
+    checkboxEl.checked = task.completed;
+    checkboxEl.addEventListener("change", function () {
+        task.completed = checkboxEl.checked;
+        updateData();
+    });
+    liEl.append(task.name);
+    liEl.append(checkboxEl);
+    listEl.append(liEl);
+    inputEl.value = "";
+}
+// read
+function readData() {
+    const myList = localStorage.getItem("myList");
+    if (myList == null) {
+        return [];
+    }
+    return JSON.parse(myList);
+}
+// update
+function updateData() {
+    localStorage.setItem("myList", JSON.stringify(tasks));
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,57 @@
-console.log('Hello Todo');
+const inputEl = document.getElementById('input')! as HTMLInputElement
+const formEl = document.querySelector('form')!
+const listEl = document.getElementById('list')!
+formEl.addEventListener('submit', saveData)
+
+interface Task {
+  name: string,
+  completed: boolean
+}
+
+const tasks:Task[]=readData()
+tasks.forEach(createList)
+
+// save
+function saveData(event:SubmitEvent) {
+  event.preventDefault()
+
+  const newTask:Task = {
+    name: inputEl.value,
+    completed: false
+  }
+  createList(newTask)
+  tasks.push(newTask)
+  localStorage.setItem("myList", JSON.stringify(tasks))
+}
+
+// createList
+function createList(task:Task) {
+  const liEl = document.createElement("li")
+  const checkboxEl = document.createElement("input") 
+
+  checkboxEl.type = "checkbox"
+  checkboxEl.checked = task.completed
+  checkboxEl.addEventListener("change", function() {
+    task.completed = checkboxEl.checked
+    updateData()
+  })
+
+  liEl.append(task.name)
+  liEl.append(checkboxEl)
+  listEl.append(liEl)
+  inputEl.value = ""
+}
+
+// read
+function readData() {
+  const myList = localStorage.getItem("myList")
+  if (myList == null) {
+    return [] 
+  }
+  return JSON.parse(myList) 
+}
+
+// update
+function updateData() {
+  localStorage.setItem("myList", JSON.stringify(tasks))
+}
